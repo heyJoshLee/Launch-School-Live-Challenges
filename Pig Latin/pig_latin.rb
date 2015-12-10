8b2f9d62ce54d1524e252bf94388054523fe4a8c
jleewebdev <lee45276@gmail.com>
2015-12-10 20:48:00 +0800

    Finished pig latin

--- Pig Latin/pig_latin.rb
+++ Pig Latin/pig_latin.rb
@@ -1,54 +1,43 @@
 class PigLatin
-
   VOWELS = %w(a e i o u)
 
   def self.translate(words)
    translated_words = []
+   dialect = "ay"
+
+   first_two_yt_xr = %w(yt xr)
+   first_two_ch_qu_th = %w(ch qu th)
+   first_three_squ_thr_sch = %w(squ thr sch)
 
     words.split(" ").each do |word|
-      
-
       if VOWELS.include?(word[0])
-
-
         translated_words << (word + "ay")
-
-         elsif word[0,2] == "yt" || word[0,2] == "xr"
-          translated_words << (word + "ay")
-
-        elsif word[0,2] == "ye"
-          first_letter = word[0]
-          word[0] = ""
-          word <<first_letter
-          translated_words << (word + "ay")
-
-            
-
-        elsif word[0,3] == "squ" || word[0,3] == "thr" || word[0,3] == "sch"
-        first_letters = word[0,3]
-        word[0,3] = ""
-        word << first_letters
+      elsif first_two_yt_xr.include? word[0,2]
         translated_words << (word + "ay")
-
-      elsif word[0,2] == "ch" || word[0,2] == "qu" || word[0,2] == "th" 
-        first_letters = word[0,2]
-        word[0,2] = ""
-        word << first_letters
-        translated_words << (word + "ay")
-
-      
+      elsif word[0,2] == "ye"
+        translated_words << (shift_letters_then_push(word) + dialect)
+      elsif first_three_squ_thr_sch.include? word[0,3]
+        translated_words << (shift_letters_then_push(word,3) + dialect)
+      elsif first_two_ch_qu_th.include? word[0,2]
+        translated_words << (shift_letters_then_push(word, 2) + dialect)
       else
-        a = word[0]
-        word[0] = ""
-        word << a
-        translated_words << (word + "ay")
+        translated_words << (shift_letters_then_push(word) + dialect)
       end
-    
     end
 
     translated_words.join(" ")
-
   end
 
-
+private
+  def self.shift_letters_then_push(word, n=0)
+    if n == 0
+      a = word[n]
+      word[n] = ""
+      word << a
+    else
+      a = word[0,n]
+      word[0,n] = ""
+      word << a
+    end
+  end
 end
