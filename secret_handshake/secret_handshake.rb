73bb4c9778702a27039d0386218ad17cf019283f
jleewebdev <lee45276@gmail.com>
2015-12-16 15:46:55 +0800

    Secret Handshake refactor, cleaned up if statmets moved directions for commands to reference COMMAND_KEY constant

--- secret_handshake/secret_handshake.rb
+++ secret_handshake/secret_handshake.rb
@@ -1,44 +1,23 @@
 class SecretHandshake
   attr_accessor :binary_num, :nums, :commands
+  COMMAND_KEY = ["wink", "double blink", "close your eyes", "jump"]
 
   def initialize num
-    if num.class.name == "Fixnum"
+    @commands = []
+    if num.class == Fixnum
       @binary_num = num.to_s 2
-      @commands = []
       convert_to_commands
-    else
-      @commands = []
-
     end
-         
-
   end
 
+  private
+
   def convert_to_commands
-    if @binary_num[-1] == "1"
-      @commands << "wink"
-    end
-
-    if @binary_num[-2] == "1"
-      @commands << "double blink"
-    end
-
-    if @binary_num[-3] == "1"
-      @commands << "close your eyes"
-    end
-
-    if @binary_num[-4] == "1"
-      @commands << "jump"
-    end
-
-    if @binary_num[-5] == "1"
-      @commands.reverse!
-    end
-    
+      @commands << COMMAND_KEY[0] if @binary_num[-1] == "1"
+      @commands << COMMAND_KEY[1] if @binary_num[-2] == "1"
+      @commands << COMMAND_KEY[2] if @binary_num[-3] == "1"
+      @commands << COMMAND_KEY[3] if @binary_num[-4] == "1"
+      @commands.reverse!          if @binary_num[-5] == "1"
   end
 
 end
-
-shs = SecretHandshake.new(1)
-shs.convert_to_commands
-p shs.commands
